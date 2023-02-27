e02ccc2c861854029961abe62bf45d3653c0d8d5
Luca Di Fazio <highliuk@gmail.com>
2023-02-27 23:34:57 +0100

    ✨ Add pages

---
+++ lib/pages/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:net_ninja_firebase/pages/home/home.dart';
+
+class WrapperPage extends StatelessWidget {
+  const WrapperPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomePage();
+  }
+}
